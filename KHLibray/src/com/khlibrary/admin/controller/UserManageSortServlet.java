a62ae3ffc170fd64fab02c576fe0de8c8f737414
yeonu-choi <75786393+yeonu-choi@users.noreply.github.com>
2021-03-17 17:11:46 +0900

    Add files via upload

---
+++ KHLibray/src/com/khlibrary/admin/controller/UserManageSortServlet.java
@@ -0,0 +1,79 @@
+package com.khlibrary.admin.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.khlibrary.common.model.vo.PageInfo;
+import com.khlibrary.member.model.service.MemberService;
+import com.khlibrary.member.model.vo.Member;
+
+/**
+ * Servlet implementation class UserManageSortServlet
+ */
+@WebServlet("/admin/usort")
+public class UserManageSortServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserManageSortServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String viewCondition = request.getParameter("viewCondition");
+		String rank = request.getParameter("rank");
+		
+		int currentPage = 1;
+		
+		if(request.getParameter("currentPage") != null) {
+			currentPage = Integer.parseInt(request.getParameter("currentPage"));
+		}
+		
+		MemberService mService = new MemberService();
+		int listCount = mService.getListCount();
+
+		int pageLimit = 10;
+		int boardLimit = 10;
+		int maxPage;
+		int startPage;
+		int endPage; 
+
+		maxPage = (int) Math.ceil((double) listCount / boardLimit);
+		startPage = (currentPage - 1) / pageLimit * pageLimit + 1;
+
+		endPage = startPage + pageLimit - 1;
+
+		if (maxPage < endPage) {
+			endPage = maxPage;
+		}
+
+		PageInfo pi = new PageInfo(currentPage, listCount, pageLimit, boardLimit, maxPage, startPage, endPage);
+		
+		List<Member> list = mService.selectSortList(pi, viewCondition, rank);
+		
+		request.setAttribute("pi", pi);
+		request.setAttribute("list", list);
+		request.getRequestDispatcher("/views/admin/userManage.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}

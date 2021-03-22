b41a703ca9d8fd260b13aeda0e28fc98e4fa16e1
doomh90 <69809438+doomh90@users.noreply.github.com>
2021-03-22 19:35:13 +0900

    Add files via upload

---
+++ KHLibray/src/com/khlibrary/board/model/vo/FAQ.java
@@ -0,0 +1,72 @@
+package com.khlibrary.board.model.vo;
+
+public class FAQ {
+	private int fNo;
+	private String f_Qcontent;
+	private String f_Acontent;
+	private String category;
+	private String status;
+	
+	public FAQ() {}
+
+	public FAQ(int fNo, String f_Qcontent, String f_Acontent, String category, String status) {
+		super();
+		this.fNo = fNo;
+		this.f_Qcontent = f_Qcontent;
+		this.f_Acontent = f_Acontent;
+		this.category = category;
+		this.status = status;
+	}
+
+	public int getfNo() {
+		return fNo;
+	}
+
+	public void setfNo(int fNo) {
+		this.fNo = fNo;
+	}
+
+	public String getF_Qcontent() {
+		return f_Qcontent;
+	}
+
+	public void setF_Qcontent(String f_Qcontent) {
+		this.f_Qcontent = f_Qcontent;
+	}
+
+	public String getF_Acontent() {
+		return f_Acontent;
+	}
+
+	public void setF_Acontent(String f_Acontent) {
+		this.f_Acontent = f_Acontent;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "FAQ [fNo=" + fNo + ", f_Qcontent=" + f_Qcontent + ", f_Acontent=" + f_Acontent + ", category="
+				+ category + ", status=" + status + "]";
+	}
+	
+	
+	
+	
+	
+	
+}

9dd6a39788f390f2bb9a2155e5cab70d34870628
kyeongminlim <80460637+kyeongminlim@users.noreply.github.com>
2021-03-24 20:05:56 +0900

    Add files via upload

--- KHLibray/src/com/khlibrary/member/model/service/MemberService.java
+++ KHLibray/src/com/khlibrary/member/model/service/MemberService.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.util.List;
 
 import static com.khlibrary.common.JDBCTemplate.*;
+import static com.khlibrary.common.JDBCTemplate.close;
+import static com.khlibrary.common.JDBCTemplate.getConnection;
 
 import com.khlibrary.common.model.vo.PageInfo;
 import com.khlibrary.member.model.dao.MemberDao;
@@ -17,8 +19,6 @@ public class MemberService {
 		
 			Connection conn = getConnection();
 					
-//			System.out.println("conn : " + conn);
-			
 			Member loginUser = new MemberDao().loginMember(conn, member); 
 			
 			close(conn);
@@ -45,7 +45,7 @@ public class MemberService {
 		// 3. 회원 정보 수정용 서비스 메서드
 		public Member updateMember(Member m) {
 			Connection conn = getConnection();
-			
+	
 			int result = new MemberDao().updateMember(conn, m);
 			
 			Member updateMember =null;
@@ -64,15 +64,15 @@ public class MemberService {
 		// 비밀번호 수정용 서비스 메소드
 		public Member updatePwd(String user_id, String user_pwd, String new_pwd) {
 			Connection conn = getConnection();
-			
+		
 			int result = new MemberDao().updatePwd(conn, user_id, user_pwd, new_pwd);
-			
+						
 			Member updateMember =null;
 			if(result > 0) {
 				commit(conn);
 				updateMember = new MemberDao().selectMember(conn, user_id);
 			} else {
-						rollback(conn);
+				rollback(conn);
 			}
 					
 			close(conn);
@@ -86,82 +86,129 @@ public class MemberService {
 			int result = new MemberDao().idCheck(conn, user_id);
 			
 			close(conn);
-			
-			
+						
 			return result;
 		}
 		
+		public Member idSearch(Member member) {
 		
+			Connection conn = getConnection();
+						
+			Member idSearch = new MemberDao().idSearch(conn, member); 
+			
+			close(conn);
+			
+			return idSearch;
+		}
+	// 비밀번호 찾기시 아이디/이메일 맞는지 확인 	
+	public Member pwdSearch(Member member) {
 		
-	/////////////////아래부터 yw ////////////////////////////////////////////////////// 	
-		
-	public int getListCount() {
 		Connection conn = getConnection();
 		
-		int listCount = new MemberDao().getListCount(conn);
+		Member pwdSearch = new MemberDao().pwdSearch(conn, member); 
 		
 		close(conn);
 		
-		return listCount;
-		
-	}
-
-	public List<Member> selectList(PageInfo pi) {
+		return pwdSearch;
+			
+		}
+	// 비밀번호 찾기 후 수정된 비밀번호 등록하기
+	public Member pwdUpdate(String user_id, String user_pwd) {
 		Connection conn = getConnection();
 		
-		List<Member> list = new MemberDao().selectList(conn, pi);
-		
-		close(conn);
-		
-		return list;
-	}
-
-	public List<Member> selectSortList(PageInfo pi, String viewCondition, String rank) {
-		Connection conn = getConnection();
-		
-		List<Member> list = new MemberDao().selectSortList(conn, pi, viewCondition, rank);
-		
-		close(conn);
-		
-		return list;
-	}
-
-	public int changeGrade(String userId, String newGrade) {
-		Connection conn = getConnection();
-		int result = new MemberDao().changeGrade(conn, userId, newGrade);
-		
+		int result = new MemberDao().pwdUpdate(conn, user_id, user_pwd);
+					
+		Member pwdUpdate =null;
 		if(result > 0) {
 			commit(conn);
+			pwdUpdate = new MemberDao().selectMember(conn, user_id);
 		} else {
 			rollback(conn);
 		}
-		return result;
-	}
-
-	public Member loginMember(String user_id) {
-		Connection conn = getConnection();
-		
-		Member m = new MemberDao().loginMember(conn, user_id); 
-		
+				
 		close(conn);
-		
-		return m;
+						
+		return pwdUpdate;
 	}
+		
+		
+		
+		
+		
+/////////////////아래부터 yw ////////////////////////////////////////////////////// 	
+		
+		public int getListCount() {
+			Connection conn = getConnection();
 
-	public int deleteMember(String user_id, String user_pwd) {
-		Connection conn = getConnection();
-		
-		int result = new MemberDao().deleteMember(conn, user_id, user_pwd);
-		
-		if(result > 0) {
-			commit(conn);
-		} else {
-			rollback(conn);
+			int listCount = new MemberDao().getListCount(conn);
+
+			close(conn);
+
+			return listCount;
+
 		}
-		
-		close(conn);
-		
-		return result;
-	}
+
+		public List<Member> selectList(PageInfo pi) {
+			Connection conn = getConnection();
+
+			List<Member> list = new MemberDao().selectList(conn, pi);
+
+			close(conn);
+
+			return list;
+		}
+
+		public List<Member> selectSortList(PageInfo pi, String viewCondition, String rank) {
+			Connection conn = getConnection();
+
+			List<Member> list = new MemberDao().selectSortList(conn, pi, viewCondition, rank);
+
+			close(conn);
+
+			return list;
+		}
+
+		public int changeGrade(String userId, String newGrade) {
+			Connection conn = getConnection();
+			int result = new MemberDao().changeGrade(conn, userId, newGrade);
+
+			if(result > 0) {
+				commit(conn);
+			} else {
+				rollback(conn);
+			}
+			return result;
+		}
+
+		public Member loginMember(String user_id) {
+			Connection conn = getConnection();
+
+			Member m = new MemberDao().loginMember(conn, user_id); 
+
+			close(conn);
+
+			return m;
+		}
+
+		public int deleteMember(String user_id, String user_pwd) {
+			Connection conn = getConnection();
+
+			int result = new MemberDao().deleteMember(conn, user_id, user_pwd);
+
+			if(result > 0) {
+				commit(conn);
+			} else {
+				rollback(conn);
+			}
+
+			close(conn);
+
+			return result;
+		}
+
 	
+	
+		
+
+
 }
